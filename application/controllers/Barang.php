a80bfe6072352cc15f3487350ba1606bc981233a
Novian Andika <45744788+Novian227@users.noreply.github.com>
2020-05-05 15:29:15 +0700

    add stok input

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -50,6 +50,7 @@ class Barang extends CI_Controller {
 		$arr_input = array(
 			'nama_barang' => $this->input->post('nama_barang'),
 			'deskripsi' => $this->input->post('deskripsi'),
+			'stok' => $this->input->post('stok'),
 		);
 
 		$this->Barang_model->insert_data($arr_input);
@@ -88,6 +89,7 @@ class Barang extends CI_Controller {
 		$arr_input = array(
 			'nama_barang' => $this->input->post('nama_barang'),
 			'deskripsi' => $this->input->post('deskripsi'),
+			'stok' => $this->input->post('stok')
 		);
 
 		$this->Barang_model->update_data($id_barang, $arr_input);
